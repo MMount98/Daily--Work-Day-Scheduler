2aa322cc3a6b95adba681cd6d321e32a5b03bc53
Michael Mount <mount.photos@gmail.com>
2023-02-18 20:55:55 -0700

    Displhyed values in local storage

--- script.js
+++ script.js
@@ -44,7 +44,15 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  // var hour9 = localStorage.getItem("hour-9");
+  $("#hour-9 .description").text(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").text(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").text(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").text(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").text(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").text(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").text(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").text(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").text(localStorage.getItem("hour-17"));
 
   // TODO: Add code to display the current date in the header of the page.
   var day = dayjs();
